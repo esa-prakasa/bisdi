f72eeb3c6f01f34df194ce2174cdce05790c35f4
Esa Prakasa <esa.prakasa@gmail.com>
2021-09-01 14:54:36 +0700

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,9 @@
+package bisdi.example.welcome;
+
+public class HelloWorld {
+    public static void main (String[] args){
+        System.out.println("(1) Hello World!! This code is written using Java!");
+        System.out.println("(2) Hello World!! This code is written using Java!");
+        System.out.println("(3) Hello World!! This code is written using Java!");
+    }
+}
